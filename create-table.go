e641d94282d17f81e625a34bfac8a6e3b42e5d09
George Maia <georgemaiaf@gmail.com>
2022-10-17 22:42:17 -0300

    feat - Valores not null para o banco

--- create-table.go
+++ create-table.go
@@ -1,4 +1,4 @@
-//package main
+package main
 
 import (
 	"database/sql"
@@ -8,38 +8,50 @@ import (
 )
 
 func main() {
-	db, err_open := sql.Open("mysql", "root:xniC6DH rZcN84bxniC6DH rZcN84b@tcp(localhost:3306)/suzana_motorcycle_parts")
+	var db *sql.DB
+	var err error
+	db, err = sql.Open("mysql", "root:xniC6DH rZcN84bxniC6DH rZcN84b@tcp(localhost:3306)/suzana_motorcycle_parts")
 
-	if err_open != nil {
-		log.Fatal(err_open.Error())
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
-	err_ping := db.Ping()
+	err = db.Ping()
 
-	if err_ping != nil {
-		log.Fatal(err_ping.Error())
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
-	_, err_exec := db.Exec(`create table if not exists Part(
-		id int not null auto_increment,
-		name varchar(500),
-		brand varchar(50),
-		value float,
-		primary key (id)
-	)`)
+	_, err = db.Exec(`
+		drop table if exists Part
+	`)
 
-	if err_exec != nil {
-		log.Fatal(err_exec.Error())
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
-	_, err_insert := db.Exec(`insert into Part (name, brand, value) values 
+	_, err = db.Exec(`
+		create table if not exists Part (
+			id int not null auto_increment,
+			name varchar(500) not null,
+			brand varchar(50) not null,
+			value float not null,
+			primary key (id)
+		)
+	`)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	_, err = db.Exec(`insert into Part (name, brand, value) values 
 		("Luva para Motociclista Dedo Longo Tam. P Material Emborrachado e Couro, Branco/ Preto", "Multilaser", 47.64),
 		("Capacete Moto R8 Pro Tork 56 Viseira Fume Preto/Vermelho", "Tork", 169.90),
 		( "Lenço de cabeça, Romacci Máscara facial Fleece máscara facial cachecol para exterior à prova de vento à prova de frio equipamento de equitação para máscara de inverno", "Romacci", 99.19)
 	`)
 
-	if err_insert != nil {
-		log.Fatal(err_insert.Error())
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
 }
